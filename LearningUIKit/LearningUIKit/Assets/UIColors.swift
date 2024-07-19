d5d7520cd3c9eeacb87b3210446cb848958773f5
Taichone <taichone.dev@gmail.com>
2024-07-19 12:54:10 +0900

    refactor: :truck: RootViewController に改名

---
+++ LearningUIKit/LearningUIKit/Assets/UIColors.swift
@@ -0,0 +1,12 @@
+//
+//  UIColors.swift
+//  LearningUIKit
+//
+//  Created by Taichi on 2024/07/19.
+//
+
+import UIKit
+
+struct UIColors {
+    static let systemMint = UIColor(red: 0.8, green: 1, blue: 1, alpha: 1)
+}
